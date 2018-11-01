708f1d39a5f5bfee9b048dae5e10b7c6c3cdd770
smoukiDev <smakfactory@gmail.com>
2018-11-01 13:23:17 +0200

    Added BookController.

---
+++ BookLibraryService/BookLibraryAPI/Controllers/BookController.cs
@@ -0,0 +1,158 @@
+﻿// <copyright file="BookController.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
+
+namespace BookLibraryAPI.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using BookLibraryBusinessLogic.Models;
+    using BookLibraryBusinessLogic.Services;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Represents a controller
+    /// for handling the requests related
+    /// to BookLibrary Service
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    // TODO: Rename route
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private const string BAD_REQUEST = "No book with such an id in the library";
+
+        /// <summary>
+        /// The book library service interface link
+        /// </summary>
+        private IBookLibrary bookLibrary;
+
+        /// <summary>
+        /// Initializes a new instance of the 
+        /// <see cref="BookController"/> class.
+        /// </summary>
+        /// <param name="bookLibrary">
+        /// The book library.
+        /// </param>
+        public BookController(IBookLibrary bookLibrary)
+        {
+            this.bookLibrary = bookLibrary;
+        }
+
+        /// <summary>
+        /// Gets all the books in library.
+        /// </summary>
+        /// <returns>Books collection</returns>
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IActionResult result = this.NotFound();
+
+            try
+            {
+                List<Book> books;
+                books = this.bookLibrary.GetLibraryBooks().ToList();
+                if (books.Count > 0)
+                {
+                    result = this.Ok(books);
+                }
+                else
+                {
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                result = this.NotFound(ex.Message);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the book by identifier.
+        /// </summary>
+        /// <param name="id">The identifier of book.</param>
+        /// <returns>Concrete book</returns>
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            IActionResult result = this.NotFound();
+
+            Book targetBook = this.bookLibrary.GetBookById(id);
+            if (targetBook != null)
+            {
+                result = this.Ok(targetBook);
+            }
+            else
+            {
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Adds the specified new book.
+        /// </summary>
+        /// <param name="newBook">The new book.</param>
+        /// <returns>
+        /// RedirectResult displaying all the books of library
+        /// </returns>
+        [HttpPost]
+        public IActionResult Add([FromBody] Book newBook)
+        {
+            this.bookLibrary.AddBook(newBook);
+            return this.Redirect("book");
+        }
+
+        /// <summary>
+        /// Updates the book by identifier.
+        /// </summary>
+        /// <param name="id">
+        /// The identifier of existed book in library.
+        /// </param>
+        /// <param name="newBook">The new book.</param>
+        /// <returns>
+        /// <see cref="IActionResult" /> result.
+        /// </returns>
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Book newBook)
+        {
+            IActionResult result = this.BadRequest(BAD_REQUEST);
+
+            if (this.bookLibrary.UpdateBook(id, newBook))
+            {
+                result = this.Ok();
+            }
+            else
+            {
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Deletes the book by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>
+        /// <see cref="IActionResult" /> result.
+        /// </returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            IActionResult result = this.BadRequest(BAD_REQUEST);
+
+            if (this.bookLibrary.RemoveBook(id))
+            {
+                result = this.Ok();
+            }
+            else
+            {
+            }
+
+            return result;
+        }
+    }
+}
--- BookLibraryService/BookLibraryAPI/Program.cs
+++ BookLibraryService/BookLibraryAPI/Program.cs
@@ -1,22 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+﻿// <copyright file="Program.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
 
 namespace BookLibraryAPI
 {
+    // TODO: Erase useless usings
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Represent entery point to program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Creates the web host builder.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns>
+        /// <see cref="IWebHostBuilder"/> object
+        /// </returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
