6d0055fbd466b3295a33860258d80320b539a4c6
smoukiDev <smakfactory@gmail.com>
2018-11-02 11:25:16 +0200

    Added AuthorModel

---
+++ BookLibraryService/BookLibraryBusinessLogic/Models/Author.cs
@@ -0,0 +1,90 @@
+﻿// <copyright file="Author.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
+
+namespace BookLibraryBusinessLogic.Models
+{
+    /// <summary>
+    /// Represents entity - Author of the book.
+    /// </summary>
+    public class Author
+    {
+        /// <summary>
+        /// Serves for book id auto generation.
+        /// </summary>
+        private static int idCounter;
+
+        /// <summary>
+        /// Initializes static members of the
+        /// <see cref="Author"/> class.
+        /// </summary>
+        static Author()
+        {
+            idCounter = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="Author"/> class.
+        /// </summary>
+        /// <param name="firstName">
+        /// Author's first name.
+        /// </param>
+        /// <param name="lastName">
+        /// Author's last name.
+        /// </param>
+        public Author(string firstName, string lastName)
+            : this(firstName, lastName, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="Author"/> class.
+        /// </summary>
+        /// <param name="firstName">
+        /// Author's first name.
+        /// </param>
+        /// <param name="lastName">
+        /// Author's last name.
+        /// </param>
+        /// <param name="nickName">
+        /// Author's nick name.
+        /// </param>
+        public Author(string firstName, string lastName, string nickName)
+        {
+            this.AuthorId = ++idCounter;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.NickName = nickName;
+        }
+
+        /// <summary>
+        /// Gets the author's identifier.
+        /// </summary>
+        public int AuthorId
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets or sets autor's first name.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets autor's last name.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets autor's nickname.
+        /// </summary>
+        /// <value>
+        /// The name of the nick.
+        /// </value>
+        public string NickName { get; set; }
+
+    }
+}
